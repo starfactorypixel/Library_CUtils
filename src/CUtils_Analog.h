11f4281418801fe6b5363840bb4a543f855528f8
Dragon-Knight <dubki4132@mail.ru>
2024-12-04 00:00:44 +0300

    Fix

--- src/CUtils_Analog.h
+++ src/CUtils_Analog.h
@@ -4,17 +4,17 @@
 
 struct volt_calc_t
 {
-	uint16_t bits;		// Разрядность АЦП, указываем как ((1 << bits) - 1)
+	uint16_t maxadc;	// Максимальное значение АЦП, указываем как ((1 << bits) - 1)
 	uint16_t vref;		// Опорное напряжение, мВ
 	uint32_t r1 = 0;	// Верхний резистор делителя, Ом
 	uint32_t r2 = 1;	// Нижний резистор делителя, Ом
-	int adj = 0;		// Коррекция в промилле
+	int16_t adj = 0;	// Коррекция в промилле
 };
 
 uint32_t VoltageCalculate(const uint32_t adc, const volt_calc_t &params)
 {
 	const uint32_t dividerCoefficient = ((params.r1 + params.r2) * 1000) / params.r2;
-	const uint32_t adcVoltage = (adc * params.vref) / params.bits;
+	const uint32_t adcVoltage = (adc * params.vref) / params.maxadc;
 	uint32_t inputVoltage = (adcVoltage * dividerCoefficient) / 1000;
 	if(params.adj != 0)
 	{
