b45f85087386d5389130f86d36fce3714bdfbc37
Dragon-Knight <dubki4132@mail.ru>
2024-12-29 01:59:04 +0300

    add `array_reverse<>()`

--- src/CUtils_String.h
+++ src/CUtils_String.h
@@ -22,6 +22,25 @@ void ArrayReverse(uint8_t *array, uint16_t length)
 	return;
 }
 
+template <typename T> 
+void array_reverse(T *array, uint8_t count)
+{
+	uint8_t i = 0;
+	uint8_t j = count - 1;
+	T temp;
+	while(i < j)
+	{
+		temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+		
+		i++;
+		j--;
+	}
+	
+	return;
+}
+
 void memcpy_reverse(uint8_t *dest, const uint8_t *src, uint16_t len)
 {
 	if(len == 0) return;
