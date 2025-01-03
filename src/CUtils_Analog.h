ab48f733716792d9035681a438c9b709492e7496
Dragon-Knight <dubki4132@mail.ru>
2025-01-03 05:17:54 +0300

    add `INACurrentCalc`

--- src/CUtils_Analog.h
+++ src/CUtils_Analog.h
@@ -11,6 +11,32 @@ struct volt_calc_t
 	int16_t adj = 0;	// Коррекция в промилле
 };
 
+// Структура расчёта тока через шунт + INA180
+struct INACurrentCalc
+{
+	uint16_t adc;		// Разрядность АЦП, указываем как (1 << bits)
+	uint32_t vref;		// Опорное напряжение, мВ
+	uint16_t shunt;		// Сопротивление шунта, мОм
+	uint16_t gain;		// Усиление INA180
+	uint32_t k;			// Рассчитанный коэффициент
+
+
+	constexpr INACurrentCalc(uint16_t adc, uint32_t vref, uint32_t shunt, uint32_t gain) : 
+		adc(adc), vref(vref), shunt(shunt), gain(gain), 
+		k( ((vref * 1000 * 1000 + ((shunt * gain * adc) / 2)) / (shunt * gain * adc)) )
+	{}
+
+	constexpr uint32_t Get_mA(uint16_t adc_value) const
+	{
+		return (adc_value * k) / 1000;
+	}
+
+	constexpr float Get_A(uint16_t adc_value) const
+	{
+		return (float)(adc_value * k) / (1000 * 1000);
+	}
+};
+
 uint32_t VoltageCalculate(const uint32_t adc, const volt_calc_t &params)
 {
 	const uint32_t dividerCoefficient = ((params.r1 + params.r2) * 1000) / params.r2;
