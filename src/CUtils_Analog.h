53edb6255e0e1bb64f478bb6abd84d378c746f85
Dragon-Knight <dubki4132@mail.ru>
2026-06-05 14:24:27 +0300

    Add `SetCorrection` in `DividerVoltageCalc` class

--- src/CUtils_Analog.h
+++ src/CUtils_Analog.h
@@ -64,42 +64,60 @@ uint32_t VoltageCalculate(const uint32_t adc, const volt_calc_t &params)
 class DividerVoltageCalc
 {
 	static constexpr uint16_t _shift = 16;
+	static constexpr uint16_t _corr_base = 10000;
 	
 	public:
-		
-		constexpr DividerVoltageCalc(uint16_t adc_bit, uint16_t vref, uint32_t r1, uint32_t r2) : 
-			_max_adc((1U << adc_bit) - 1U), _vref(vref), _r1(r1), _r2(r2), 
+	
+		constexpr DividerVoltageCalc(uint16_t adc_bit, uint16_t vref, uint32_t r1, uint32_t r2, int16_t correction = 0) : 
+			_max_adc((1U << adc_bit) - 1U), _vref(vref), _r1(r1), _r2(r2), _correction(_corr_base + correction), 
 			_k( (((uint64_t)vref * (r1 + r2) << _shift) + (r2 * _max_adc) / 2) / (r2 * _max_adc) )
 		{}
 		
+		// Разрядность АЦП
 		void SetAdcBits(uint8_t adc_bit)
 		{
 			_max_adc = ((1U << adc_bit) - 1U);
 		}
 		
+		// Опорное напряжение, мВ
 		void SetVref(uint16_t vref)
 		{
 			_vref = vref;
 		}
 		
+		// Верхний резистор, Ом
 		void SetR1(uint32_t r1)
 		{
 			_r1 = r1;
 		}
 		
+		// Нижний резистор, Ом
 		void SetR2(uint32_t r2)
 		{
 			_r2 = r2;
 		}
 
+		// Поправка задаётся в сотых долях процента (0.01%)
+		// '0' -> без коррекции (100.00%), '125' -> +1.25%, '-50' -> -0.50%
+		void SetCorrection(int16_t correction)
+		{
+			_correction = _corr_base + correction;
+		}
+		
+		// Пересчёт коэфицента. Вызывать при любом вызове Set метода
 		void ReCalc()
 		{
 			_k = (((uint64_t)_vref * (_r1 + _r2) << _shift) + (_r2 * _max_adc) / 2) / (_r2 * _max_adc);
 		}
 		
+		// Получить значение в мВ
 		inline uint32_t Getmv(uint32_t adc) const
 		{
-			return (adc * _k) >> _shift;
+			uint64_t mv = ((uint64_t)adc * _k) >> _shift;
+			if(_correction != _corr_base)
+				mv = (mv * _correction + (_corr_base / 2)) / _corr_base;
+			
+			return (uint32_t)mv;
 		}
 		
 	private:
@@ -108,6 +126,7 @@ class DividerVoltageCalc
 		uint16_t _vref;
 		uint32_t _r1;
 		uint32_t _r2;
+		uint16_t _correction;
 		uint32_t _k;
 };
 
