f24840b7202e3f224164815663d89f2280b83512
Dragon-Knight <dubki4132@mail.ru>
2026-06-05 15:26:30 +0300

    1. Fix `DividerVoltageCalc`; 2. Add `INACurrentCalc`; 3. Cleanup;

--- src/CUtils_Analog.h
+++ src/CUtils_Analog.h
@@ -1,65 +1,83 @@
 #pragma once
 #include <inttypes.h>
 
-
-struct [[deprecated("Use DividerVoltageCalc class")]] volt_calc_t
+class INACurrentCalc
 {
-	uint16_t maxadc;	// Максимальное значение АЦП, указываем как ((1 << bits) - 1)
-	uint16_t vref;		// Опорное напряжение, мВ
-	uint32_t r1;		// Верхний резистор делителя, Ом
-	uint32_t r2;		// Нижний резистор делителя, Ом
-	int16_t adj;		// Коррекция в промилле
-
-	volt_calc_t(uint16_t m, uint16_t v, uint32_t r1_, uint32_t r2_, int16_t a)
-        : maxadc(m), vref(v), r1(r1_), r2(r2_), adj(a) {}
-};
-
-// Структура расчёта тока через шунт + INA180
-struct INACurrentCalc
-{
-	uint16_t adc;		// Разрядность АЦП, указываем как (1 << bits)
-	uint32_t vref;		// Опорное напряжение, мВ
-	uint16_t shunt;		// Сопротивление шунта, мОм
-	uint16_t gain;		// Усиление INA180
-	uint32_t k;			// Рассчитанный коэффициент
-
-
-	constexpr INACurrentCalc(uint16_t adc, uint32_t vref, uint32_t shunt, uint32_t gain) : 
-		adc(adc), vref(vref), shunt(shunt), gain(gain), 
-		k( ((vref * 1000 * 1000 + ((shunt * gain * adc) / 2)) / (shunt * gain * adc)) )
-	{}
-
-	constexpr uint32_t Get_mA(uint16_t adc_value) const
-	{
-		return (adc_value * k) / 1000;
-	}
-
-	constexpr float Get_A(uint16_t adc_value) const
-	{
-		return (float)(adc_value * k) / (1000 * 1000);
-	}
-};
-
-[[deprecated("Use DividerVoltageCalc class")]]
-uint32_t VoltageCalculate(const uint32_t adc, const volt_calc_t &params)
-{
-	const uint32_t dividerCoefficient = ((params.r1 + params.r2) * 1000) / params.r2;
-	const uint32_t adcVoltage = (adc * params.vref) / params.maxadc;
-	uint32_t inputVoltage = (adcVoltage * dividerCoefficient) / 1000;
-	if(params.adj != 0)
-	{
-		inputVoltage = (inputVoltage * (1000 + params.adj)) / 1000;
-	}
+	static constexpr uint16_t _shift = 16;
+	static constexpr uint16_t _corr_base = 10000;
 	
-	return inputVoltage;
-}
-
-
-
-
-
-
-
+	public:
+		
+		constexpr INACurrentCalc(uint16_t adc_bit, uint16_t vref, uint16_t shunt, uint16_t gain, int16_t correction = 0) : 
+			_max_adc((1U << adc_bit) - 1U), _vref(vref), _shunt(shunt), _gain(gain), _correction(_corr_base + correction), 
+			_k( ((((uint64_t)vref * 1000ULL * 1000ULL) << _shift) + ((uint64_t)shunt * gain * _max_adc) / 2) / ((uint64_t)shunt * gain * _max_adc) )
+		{}
+		
+		// Разрядность АЦП
+		void SetAdcBits(uint8_t adc_bit)
+		{
+			_max_adc = ((1U << adc_bit) - 1U);
+		}
+		
+		// Опорное напряжение, мВ
+		void SetVref(uint16_t vref)
+		{
+			_vref = vref;
+		}
+		
+		// Сопротивление шунта, мОм
+		void SetShunt(uint16_t shunt)
+		{
+			_shunt = shunt;
+		}
+		
+		// Коэффициент усиления INA
+		void SetGain(uint16_t gain)
+		{
+			_gain = gain;
+		}
+		
+		// Поправка задаётся в сотых долях процента (0.01%)
+		// '0' -> без коррекции (100.00%), '125' -> +1.25%, '-50' -> -0.50%
+		void SetCorrection(int16_t correction)
+		{
+			_correction = _corr_base + correction;
+		}
+		
+		// Пересчёт коэффициента. Вызывать при любом вызове Set метода
+		void ReCalc()
+		{
+			_k = ((((uint64_t)_vref * 1000ULL * 1000ULL) << _shift) + ((uint64_t)_shunt * _gain * _max_adc) / 2) / ((uint64_t)_shunt * _gain * _max_adc);
+		}
+		
+		inline uint32_t Get_mA(uint16_t adc_value) const
+		{
+			return (uint32_t)(_Calculate(adc_value) / 1000ULL);
+		}
+		
+		inline float Get_A(uint16_t adc_value) const
+		{
+			return (float)(_Calculate(adc_value) / 1000000.0f);
+		}
+		
+	private:
+		
+		inline uint32_t _Calculate(uint16_t adc_value) const
+		{
+			uint64_t current_uA = ((uint64_t)adc_value * _k) >> _shift;
+			if(_correction != _corr_base)
+				current_uA = (current_uA * _correction + (_corr_base / 2)) / _corr_base;
+			
+			return (uint32_t)current_uA;
+		}
+		
+		uint16_t _max_adc;
+		uint16_t _vref;
+		uint16_t _shunt;
+		uint16_t _gain;
+		uint16_t _correction;
+		uint32_t _k;
+};
 
 class DividerVoltageCalc
 {
@@ -104,23 +122,34 @@ class DividerVoltageCalc
 			_correction = _corr_base + correction;
 		}
 		
-		// Пересчёт коэфицента. Вызывать при любом вызове Set метода
+		// Пересчёт коэффициента. Вызывать при любом вызове Set метода
 		void ReCalc()
 		{
 			_k = (((uint64_t)_vref * (_r1 + _r2) << _shift) + (_r2 * _max_adc) / 2) / (_r2 * _max_adc);
 		}
 		
 		// Получить значение в мВ
-		inline uint32_t Getmv(uint32_t adc) const
+		inline uint32_t GetmV(uint32_t adc_value) const
 		{
-			uint64_t mv = ((uint64_t)adc * _k) >> _shift;
-			if(_correction != _corr_base)
-				mv = (mv * _correction + (_corr_base / 2)) / _corr_base;
-			
-			return (uint32_t)mv;
+			return (uint32_t)_Calculate(adc_value);
+		}
+
+		// Получить значение в мВ
+		inline float GetV(uint32_t adc_value) const
+		{
+			return (float)(_Calculate(adc_value) / 1000.0f);
 		}
 		
 	private:
+		
+		inline uint32_t _Calculate(uint16_t adc_value) const
+		{
+			uint64_t voltage_mV = ((uint64_t)adc_value * _k) >> _shift;
+			if(_correction != _corr_base)
+				voltage_mV = (voltage_mV * _correction + (_corr_base / 2)) / _corr_base;
+			
+			return (uint32_t)voltage_mV;
+		}
 		
 		uint16_t _max_adc;
 		uint16_t _vref;
@@ -129,4 +158,3 @@ class DividerVoltageCalc
 		uint16_t _correction;
 		uint32_t _k;
 };
-
