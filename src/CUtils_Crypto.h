ef952b4b409c6d251c56ef09d57d317d9c116e66
Dragon-Knight <dubki4132@mail.ru>
2026-08-17 18:56:23 +0300

    Fix `GetSerialNumber64` for ESP32

--- src/CUtils_Crypto.h
+++ src/CUtils_Crypto.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <inttypes.h>
 
+#ifdef HAL_MODULE_ENABLED
+
 void GetSerialNumber64(uint8_t *sn)
 {
 	const uint32_t *uid = (const uint32_t *)UID_BASE;
@@ -10,3 +12,24 @@ void GetSerialNumber64(uint8_t *sn)
 	serial[1] = uid[1];
 	memcpy(sn, serial, sizeof(serial));
 }
+
+#endif
+
+#ifdef ESP32
+
+void GetSerialNumber64(uint8_t *sn)
+{
+	uint8_t mac[6];
+	esp_efuse_mac_get_default(mac);
+	
+	sn[0] = mac[0];
+	sn[1] = mac[1];
+	sn[2] = mac[2];
+	sn[3] = mac[3];
+	sn[4] = mac[4];
+	sn[5] = mac[5];
+	sn[6] = mac[0] ^ mac[2] ^ mac[4];
+	sn[7] = mac[1] ^ mac[3] ^ mac[5];
+}
+
+#endif
